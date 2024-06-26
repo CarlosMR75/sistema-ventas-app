e54599413dca7da812eaf8d6d4bda3e67a153ee7
CarlosMR75 <carlosemr75@gmail.com>
2024-06-25 20:30:35 -0600

    Configuracion swaggerui

--- sistema-ventas-api/src/routes/index.routes.ts
+++ sistema-ventas-api/src/routes/index.routes.ts
@@ -8,11 +8,62 @@ class IndexRoutes {
         this.router = Router();
         this.config();
     }
-
     config(): void {
+        /**
+         * @swagger
+         * /api:
+         *  get:
+         *      tags: [index]
+         *      summary: Default index
+         *      desription: Ruta por defecto de la API
+         *      produces:
+         *          - application/json
+         *      responses:
+         *          200:
+         *              description: Exitoso
+         */
         this.router.get('/', indexController.index);
+        /**
+         * @swagger
+         * /api:
+         *  post:
+         *      tags: [index]
+         *      summary: Default index
+         *      desription: Ruta por defecto de la API
+         *      produces:
+         *          - application/json
+         *      responses:
+         *          200:
+         *              description: Exitoso
+         */
         this.router.post('/', indexController.insert);
+        /**
+         * @swagger
+         * /api:
+         *  put:
+         *      tags: [index]
+         *      summary: Default index
+         *      desription: Ruta por defecto de la API
+         *      produces:
+         *          - application/json
+         *      responses:
+         *          200:
+         *              description: Exitoso
+         */
         this.router.put('/', indexController.update);
+        /**
+         * @swagger
+         * /api:
+         *  delete:
+         *      tags: [index]
+         *      summary: Default index
+         *      desription: Ruta por defecto de la API
+         *      produces:
+         *          - application/json
+         *      responses:
+         *          200:
+         *              description: Exitoso
+         */
         this.router.delete('/', indexController.delete);
     }
 }
